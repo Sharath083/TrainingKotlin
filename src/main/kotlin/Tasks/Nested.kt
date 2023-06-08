a3f95061efe8e0d21b193acf42ba9d0653a34698
Sharath083 <sharath.r@marketsimplified.com>
2023-06-08 17:21:05 +0530

    commit

---
+++ src/main/kotlin/Tasks/Nested.kt
@@ -0,0 +1,29 @@
+package Tasks
+
+class Nested {
+    private var num:Int=1
+    fun outer(){
+        println("outer")
+    }
+    class Inner(){
+        private var num2:Int=2
+        fun innerFun(){
+            println("Inner")
+        }
+        class Inner2(){
+            fun inner2(){
+                println("inner2")
+//                var s:Int=num+num1
+            }
+        }
+    }
+
+}
+fun main(){
+    var ob=Nested()
+    var ob1= Nested.Inner()
+    var ob2=Nested.Inner.Inner2()
+    ob.outer()
+    ob1.innerFun()
+    ob2.inner2()
+}
---
+++ src/main/kotlin/Tasks/SealedClass.kt
@@ -0,0 +1,26 @@
+package Tasks
+
+sealed class Shape{
+    class Circle(var radius: Float): Shape()
+    class Square(var length: Int): Shape()
+    class Rectangle(var length: Int, var breadth: Int): Shape()
+    //  object NotAShape : Shape()
+}
+
+fun eval(e: Shape) =
+    when (e) {
+        is Shape.Circle ->println("Circle  ${3.14*e.radius*e.radius}")
+        is Shape.Square ->println("Square  ${e.length*e.length}")
+        is Shape.Rectangle ->println("Rectagle  ${e.length*e.breadth}")
+
+    }
+fun main(args: Array<String>) {
+
+    var circle = Shape.Circle(5.0f)
+    var square = Shape.Square(5)
+    var rectangle = Shape.Rectangle(4,5)
+
+    eval(circle)
+    eval(square)
+    eval(rectangle)
+}
