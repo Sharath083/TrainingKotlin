f39e58bac9e659e632cbf35d9d4f2442269cb3d8
Sharath083 <sharath.r@marketsimplified.com>
2023-06-12 15:22:41 +0530

    commit

--- src/main/kotlin/Tasks/CaesarCipher.kt
+++ src/main/kotlin/Tasks/CaesarCipher.kt
@@ -2,29 +2,33 @@ package Tasks
 
 import java.util.*
 
-fun shiftFun(s:Char, l:Int):Char {
-    var d: Char = s
-    var l1:Int=l
 
-    while(l1 != 0){
 
-        if (d != 'z'&& d!='Z') {
-            d++
-            l1--
+    fun shiftFun(s: Char, l: Int): Char {
+        var d: Char = s
+        var l1: Int = l
+
+        while (l1 != 0) {
+
+            if (d != 'z' && d != 'Z') {
+                d++
+                l1--
+            } else if (d == 'z') {
+                d = 'a'
+                l1 -= 1
+            } else {
+                d = 'A'
+                l1 -= 1
+            }
         }
-        else if(d=='z') {
-            d='a'
-            l1 -= 1
-        }
-        else{
-            d='A'
-            l1 -= 1
-        }
+
+        return d
     }
 
-    return  d
-}
+
+
 fun main(){
+
     var reader = Scanner(System.`in`)
     println("Enter a string")
     var inputString:String? = readlnOrNull()
---
+++ src/main/kotlin/Tasks/Singleton_CompanionObject.kt
@@ -0,0 +1,35 @@
+package Tasks
+
+object SingletonDemo { //syntax to create singleton
+    var variable="SingletonVariable"
+
+    fun single(){
+        println("Singleton Method")
+    }
+
+}
+//fun main(){
+//    SingletonDemo.single()
+//    println(SingletonDemo.variable)
+//    SingletonDemo.variable="new"
+//    println(SingletonDemo.variable)
+//
+//}
+
+class Comp{
+    fun normal(){
+        println("Normal Method")
+    }
+    companion object{
+        var co="Co Variable"
+        fun co(){
+            println("Companion ob method")
+        }
+    }
+}
+fun main(){
+    var ob=Comp()
+    ob.normal()//we have to create object to access methods outside com Object
+    println(Comp.co)//can access using class name like static in java
+    Comp.co()
+}
